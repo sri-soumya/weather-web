f48da3920e2fb6fd7aeef54787bb12568a86153b
soumya <sri.soumya0607@gmail.com>
2020-11-19 12:09:28 +0530

    heroku setup

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const forecast = require("./utils/forecast");
 
 const app= express();
 
+const port= process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname,"../public")));
 app.set("view engine","hbs");
 app.set("views", path.join(__dirname,"../templates/views"));
@@ -77,6 +78,6 @@ app.get('*',(req,res)=>{
 });
 
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("Server is running");
  });
